5136d72092e54eb7bb29067da6c40c7201690a94
DevKumar00001 <appdeveloper400@gmail.com>
2023-10-18 06:43:46 +0530

    feat: Added file name validator

--- app/src/main/java/com/dragon/ide/ui/dialogs/filemanager/CreateFileDialog.java
+++ app/src/main/java/com/dragon/ide/ui/dialogs/filemanager/CreateFileDialog.java
@@ -1,8 +1,12 @@
 package com.dragon.ide.ui.dialogs.filemanager;
 
 import android.app.Activity;
+import android.text.Editable;
+import android.text.TextWatcher;
+import android.widget.Toast;
 import com.dragon.ide.R;
 import com.dragon.ide.databinding.LayoutNewFileDialogBinding;
+import com.dragon.ide.utils.FileNameValidator;
 import com.google.android.material.dialog.MaterialAlertDialogBuilder;
 
 public class CreateFileDialog extends MaterialAlertDialogBuilder {
@@ -15,7 +19,37 @@ public class CreateFileDialog extends MaterialAlertDialogBuilder {
     setTitle(activity.getString(R.string.create_new_file));
     binding = LayoutNewFileDialogBinding.inflate(activity.getLayoutInflater());
     setView(binding.getRoot());
-    setPositiveButton(R.string.create, (param1, param2) -> {});
+    binding.fileName.addTextChangedListener(
+        new TextWatcher() {
+          @Override
+          public void beforeTextChanged(CharSequence arg0, int arg1, int arg2, int arg3) {}
+
+          @Override
+          public void onTextChanged(CharSequence arg0, int arg1, int arg2, int arg3) {
+            binding.TextInputLayout1.setError(activity.getString(R.string.invalid_file_name));
+            if (FileNameValidator.isValidFileName(arg0.toString())) {
+              binding.TextInputLayout1.setErrorEnabled(false);
+            } else {
+              binding.TextInputLayout1.setErrorEnabled(true);
+            }
+          }
+
+          @Override
+          public void afterTextChanged(Editable arg0) {}
+        });
+    setPositiveButton(
+        R.string.create,
+        (param1, param2) -> {
+          if (FileNameValidator.isValidFileName(binding.fileName.getText().toString())) {
+
+          } else {
+            Toast.makeText(
+                    getContext(),
+                    getContext().getString(R.string.invalid_file_name),
+                    Toast.LENGTH_SHORT)
+                .show();
+          }
+        });
     setNegativeButton(R.string.cancel, (param1, param2) -> {});
   }
 }
---
+++ app/src/main/java/com/dragon/ide/utils/FileNameValidator.java
@@ -0,0 +1,12 @@
+package com.dragon.ide.utils;
+
+import java.util.regex.Pattern;
+
+public class FileNameValidator {
+  private static final String FILE_NAME_PATTERN = "^[a-zA-Z0-9-_\\.]+$";
+  private static final Pattern pattern = Pattern.compile(FILE_NAME_PATTERN);
+
+  public static boolean isValidFileName(String fileName) {
+    return pattern.matcher(fileName).matches();
+  }
+}
