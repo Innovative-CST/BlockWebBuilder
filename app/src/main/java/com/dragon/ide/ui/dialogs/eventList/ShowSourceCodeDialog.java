112ba1ae6249d6590364fad0a5b280c98255a6c7
CyberGlitch01 <kadev70@gmail.com>
2023-10-25 15:52:11 +0530

    fix: Changed active line color in editor

--- app/src/main/java/com/dragon/ide/ui/dialogs/eventList/ShowSourceCodeDialog.java
+++ app/src/main/java/com/dragon/ide/ui/dialogs/eventList/ShowSourceCodeDialog.java
@@ -1,12 +1,13 @@
 package com.dragon.ide.ui.dialogs.eventList;
 
 import android.app.Activity;
+import android.graphics.Color;
 import com.dragon.ide.R;
 import com.dragon.ide.databinding.LayoutSouceCodeDialogBinding;
-import com.dragon.ide.objects.WebFile;
 import com.google.android.material.dialog.MaterialAlertDialogBuilder;
 import editor.tsd.tools.Themes;
 import editor.tsd.widget.CodeEditorLayout;
+import io.github.rosemoe.sora.widget.schemes.EditorColorScheme;
 
 public class ShowSourceCodeDialog extends MaterialAlertDialogBuilder {
   public ShowSourceCodeDialog(Activity activity, String code, String language) {
@@ -18,6 +19,7 @@ public class ShowSourceCodeDialog extends MaterialAlertDialogBuilder {
     binding.editor.setCode(code);
     binding.editor.setTheme(Themes.SoraEditorTheme.Light.Quietlight);
     binding.editor.setLanguageMode(language);
+    binding.editor.getSoraCodeEditor().getColorScheme().setColor(EditorColorScheme.CURRENT_LINE, Color.parseColor("#11000000"));
     setView(binding.getRoot());
     setPositiveButton(R.string.cancel, (param1, param2) -> {});
   }
