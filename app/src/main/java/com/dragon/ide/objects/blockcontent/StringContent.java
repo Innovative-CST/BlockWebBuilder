e7128f27ec60ad866f6ee02f9170801c5f949c9d
DevKumar00001 <appdeveloper400@gmail.com>
2023-11-04 13:44:37 +0530

    fix: Added deep clone in StringContent

--- app/src/main/java/com/dragon/ide/objects/blockcontent/StringContent.java
+++ app/src/main/java/com/dragon/ide/objects/blockcontent/StringContent.java
@@ -25,6 +25,56 @@ public class StringContent extends ComplexBlockContent implements Cloneable {
 
   @Override
   public StringContent clone() throws CloneNotSupportedException {
-    return (StringContent) super.clone();
+    StringContent mComplexBlockContent = new StringContent();
+    String mValue;
+    if (getValue() != null) {
+      mValue = new String(getValue());
+    } else {
+      mValue = new String("");
+    }
+    String mId;
+    if (getId() != null) {
+      mId = new String(getId());
+    } else {
+      mId = new String();
+    }
+    String mSurrounder;
+    if (getSurrounder() != null) {
+      mSurrounder = new String(getSurrounder());
+    } else {
+      mSurrounder = new String("");
+    }
+    int mType;
+    if (getType() == 0) {
+      mType = new Integer(getType());
+    } else {
+      mType = 0;
+    }
+    String mAcceptance;
+    if (getAcceptance() != null) {
+      mAcceptance = new String(getAcceptance());
+    } else {
+      mAcceptance = new String("");
+    }
+    boolean mSupportCodeEditor;
+    mSupportCodeEditor = new Boolean(getSupportCodeEditor());
+    int onClick;
+    onClick = new Integer(getOnClick());
+    String mText;
+    if (getText() != null) {
+      mText = new String(getText());
+    } else {
+      mText = new String("");
+    }
+
+    mComplexBlockContent.setText(mText);
+    mComplexBlockContent.setValue(mValue);
+    mComplexBlockContent.setId(mId);
+    mComplexBlockContent.setSurrounder(mSurrounder);
+    mComplexBlockContent.setType(mType);
+    mComplexBlockContent.setAcceptance(mAcceptance);
+    mComplexBlockContent.setSupportCodeEditor(mSupportCodeEditor);
+
+    return mComplexBlockContent;
   }
 }
