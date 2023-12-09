85414f4c199440585c57c7944802a2b12eb54712
DevKumar00001 <appdeveloper400@gmail.com>
2023-12-09 13:07:23 +0530

    feat: Reftesh list after adding new Events

--- app/src/main/java/com/dragon/ide/ui/activities/EventListActivity.java
+++ app/src/main/java/com/dragon/ide/ui/activities/EventListActivity.java
@@ -128,6 +128,10 @@ public class EventListActivity extends BaseActivity {
                                 }
                               }
                             }
+                            runOnUiThread(
+                                () -> {
+                                  showEventList();
+                                });
                           });
                     }
                   });
