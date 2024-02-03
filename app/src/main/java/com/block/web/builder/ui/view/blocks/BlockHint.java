5481bf7da92c564bd91f3eb33ef467f3a02ddebb
DevKumar00001 <96680248+DevKumar00001@users.noreply.github.com>
2024-02-03 08:41:54 +0530

    feat(BlockHint): Use colorSurfaceInverse instead of Black for BlockHint

--- app/src/main/java/com/block/web/builder/ui/view/blocks/BlockHint.java
+++ app/src/main/java/com/block/web/builder/ui/view/blocks/BlockHint.java
@@ -1,10 +1,10 @@
 package com.block.web.builder.ui.view.blocks;
 
 import android.content.Context;
-import android.graphics.Color;
 import android.graphics.PorterDuff;
 import android.graphics.drawable.Drawable;
 import android.widget.LinearLayout;
+import com.block.web.builder.utils.ColorUtils;
 
 public class BlockHint extends LinearLayout {
   public BlockHint(Context context, int resource) {
@@ -13,14 +13,15 @@ public class BlockHint extends LinearLayout {
     setTag("shadow");
 
     Drawable backgroundDrawable = getResources().getDrawable(resource, null);
-    backgroundDrawable.setTint(Color.BLACK);
+    backgroundDrawable.setTint(
+        ColorUtils.getColor(context, com.google.android.material.R.attr.colorSurfaceInverse));
     backgroundDrawable.setTintMode(PorterDuff.Mode.SRC_IN);
     setBackground(backgroundDrawable);
   }
 
   public void setBlockResource(int resource) {
     Drawable backgroundDrawable = getResources().getDrawable(resource, null);
-    backgroundDrawable.setTint(Color.BLACK);
+    backgroundDrawable.setTint(ColorUtils.getColor(getContext(), com.google.android.material.R.attr.colorSurfaceInverse));
     backgroundDrawable.setTintMode(PorterDuff.Mode.SRC_IN);
     setBackground(backgroundDrawable);
   }
